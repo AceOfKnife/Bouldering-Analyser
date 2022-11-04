85711ee5c0d9f010ff5c1b63e92f02fa01ae0be0
Vincent Nguyen <vincent.nguyen@hotmail.co.uk>
2022-11-04 00:31:00 +0000

    Initial Commit

---
+++ Bouldering Analyser/Bouldering_AnalyserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bouldering_AnalyserApp.swift
+//  Bouldering Analyser
+//
+//  Created by Vincent Nguyen on 04/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Bouldering_AnalyserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
