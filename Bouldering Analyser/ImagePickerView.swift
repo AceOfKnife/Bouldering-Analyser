378c166808be61f65b869f359300537890b483cb
Vincent Nguyen <vincent.nguyen@hotmail.co.uk>
2023-04-30 17:22:51 +0100

    Added credits

--- Bouldering Analyser/ImagePickerView.swift
+++ Bouldering Analyser/ImagePickerView.swift
@@ -4,13 +4,20 @@ import SwiftUI
 /**
  # Image Picker View
  This *View* allows users to upload their own images on their device by either taking a photo or uploading one from their library.
+ Credit: https://www.hackingwithswift.com/books/ios-swiftui/importing-an-image-into-swiftui-using-phpickerviewcontroller
  */
 struct ImagePickerView: UIViewControllerRepresentable {
 
+    // The variable storing the selected image
     @Binding var selectedImage: UIImage?
+    
+    // Using a presentation mode to overlay the other pages
     @Environment(\.presentationMode) var isPresented
+    
+    // Variable storing whether its a camera or photo upload
     var sourceType: UIImagePickerController.SourceType
         
+    // Functions that are required for the UIViewController protocol
     func makeUIViewController(context: Context) -> UIImagePickerController {
         let imagePicker = UIImagePickerController()
         imagePicker.sourceType = self.sourceType
@@ -26,6 +33,7 @@ struct ImagePickerView: UIViewControllerRepresentable {
     }
 }
 
+// Class that initialises the image picking system
 class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     var picker: ImagePickerView
     
